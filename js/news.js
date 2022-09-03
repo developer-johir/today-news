cdbb39882173962fc997a216da4278b3f198cfb0
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-09-03 10:25:15 +0600

    dynamic category

--- js/news.js
+++ js/news.js
@@ -1,1 +1,39 @@
-console.log('Jahirul Islam');
+const loadCategory = () => {
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(data => displayCategory(data.data.news_category))
+}
+
+const displayCategory = categorys => {
+    //console.log(categorys.data.news_category[0].category_name);
+    const categoryContainer = document.getElementById('category-container');
+    // for(const category in categorys){
+    // }
+
+
+    categorys.forEach(category => {
+        const categoryli = document.createElement('li');
+    categoryli.innerHTML = `
+        <li class="nav-item">
+            <a class="nav-link active px-3" aria-current="page" href="#">${category.category_name}</a>
+        </li>
+    `
+    categoryContainer.appendChild(categoryli);
+    })
+
+
+    
+    // const categoryli = document.createElement('li');
+    // categoryli.innerHTML = `
+    //     <li class="nav-item">
+    //         <a class="nav-link active" aria-current="page" href="#">${categorys.data.news_category[0].category_name}</a>
+    //     </li>
+    // `
+    // categoryContainer.appendChild(categoryli);
+}
+
+loadCategory();
+
+
+
+//console.log(categorys.data.news_category[0].category_name);
---
+++ mo-35.5-es6-api-recap/js/app.js
@@ -0,0 +1,38 @@
+// // Practice-1
+// const student = {
+//     name: 'abul',
+//     age: 15,
+//     identity: {id: 3, fathersName: 'babul', mathersName: 'bubli', home: 'sunamganj' },
+//     subject: ['bangla', 'english', 'math', 'arbi', 'urdu'],
+//     aport: function (){
+//         return this.name;
+//     }
+// }
+
+// const output = student.aport();
+// console.log(output);
+
+
+// Practice-2
+// pore korbo.......
+
+// Practice-2
+const add1 = () => 89;
+//console.log(add1()); 
+
+const add2 = (a) => a / 17;
+//console.log(add2(120));
+
+const add3 = (a, b) => {
+    const sum = a + b;
+    const suma = sum / 2;
+    return suma;
+}
+//console.log(add3(50, 60));
+
+const add4 = (a, b, c, d, e){
+    const sum1 = a + b;
+    const sum2 = b + c;
+    const sum3 = c + d;
+    const sum4 = d + d;
+}
