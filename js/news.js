e834e040500d24ca86505e374503437fae6bdd38
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-09-04 11:15:57 +0600

    sort news add

--- js/news.js
+++ js/news.js
@@ -45,7 +45,7 @@ const displayNews = newse => {
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('card');
         newsDiv.innerHTML = `
-        <div onclick="loadModalDetail('${news.news_id}')" data-bs-toggle="modal" data-bs-target="#newsDetailModal" class="row g-0 p-3">
+        <div data-bs-toggle="modal" data-bs-target="#newsDetailModal" class="row g-0 p-3">
             <div class="col-md-4">
                 <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
                 </div>
@@ -114,48 +114,42 @@ const items = [
 items.sort((a, b) => b.value-a.value);
 console.log(items);
 
-// const loadModalDetail = async news_id =>{
-//     const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
-//     const res = await fetch(url);
-//     const data = await res.json();
-//     console.log(data.data);
-// }
-
-
 
 const loadModalDetail = (news_id) => {
     const url = `https://openapi.programming-hero.com/api/news/${news_id}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data.data))
+    .then(data => displayModal(data.data))
 }
 
-// const displayModal = modals => {
-//     console.log(modals)
-//     const newsDetailModal = document.getElementById('modalDetail');
-//     modals.forEach(modal => {
-//         const modalDiv = document.createElement('div')
-//     modalDiv.classList.add('modal')
-//     modalDiv.innerHTML = `
-//     <div class="modal fade" id="newsDetailModal" tabindex="-1" aria-labelledby="newsDetailModalLabel" aria-hidden="true">
-//       <div class="modal-dialog">
-//         <div class="modal-content">
-//           <div class="modal-header">
-//             <h5 class="modal-title" id="newsDetailModalLabel">Modal title</h5>
-//             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//           </div>
-//           <div class="modal-body">
-//             ...
-//           </div>
-//           <div class="modal-footer">
-//             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//     `
-//     newsDetailModal.appendChild(modalDiv)
-//     })
-// }
+const displayModal = modals => {
+    console.log(modals)
+    const newsDetailModal = document.getElementById('modalDetail');
+    modals.forEach(modal => {
+        const modalDiv = document.createElement('div')
+    modalDiv.classList.add('modal')
+    modalDiv.innerHTML = `
+    <div class="modal fade" id="newsDetailModal" tabindex="-1"           aria-labelledby="newsDetailModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="newsDetailModalLabel">Modal title</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+    newsDetailModal.appendChild(modalDiv)
+    })
+}
+ loadModalDetail()
+// onclick="loadModalDetail('${news.news_id}')"
 
 loadCategory();
