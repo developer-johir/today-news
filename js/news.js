094632a5656d556b92ed086cd22b5cda5712f3d9
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-09-04 06:00:52 +0600

    modal adds

--- js/news.js
+++ js/news.js
@@ -25,13 +25,30 @@ const loadNewsDetail = (category_id) => {
 }
 
 const displayNews = newse => {
-    console.log(newse);
+    //console.log(newse);
     const newsContainer = document.getElementById('news-container');
+    newsContainer.textContent = '';
+    if(newse.length === 0){
+        const noFound = document.getElementById('no-found');
+        noFound.classList.remove('d-none');
+    }
+    else{
+        const noFound = document.getElementById('no-found');
+        noFound.classList.add('d-none');
+    }
+    if(newse.length !== 0){
+        const sumItems = document.getElementById('sum-items');
+        sumItems.innerText = newse;
+    }
+    else{
+        const sumItems = document.getElementById('sum-items');
+        sumItems.innerText = newse.length;
+    }
     newse.forEach(news => {
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('card');
         newsDiv.innerHTML = `
-        <div class="row g-0 p-3">
+        <div onclick="loadModalDetail('${news.news_id}')" data-bs-toggle="modal" data-bs-target="#newsDetailModal" class="row g-0 p-3">
             <div class="col-md-4">
                 <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
                 </div>
@@ -63,7 +80,7 @@ const displayNews = newse => {
                     <i class="fa-regular fa-star"></i>
                   </div>
                   <div class="col mt-3">
-                    <a href=""><i class="fa-solid fa-arrow-right text-primary"></i></a>
+                    <button class="border-0 bg-white" href=""><i class="fa-solid fa-arrow-right text-primary"></i></button>
                   </div>
                 </div>
               </div>
@@ -73,9 +90,50 @@ const displayNews = newse => {
         `
         newsContainer.appendChild(newsDiv);
     })
-    //for(const news in newse){}
-    
-    
 }
+
+// const loadModalDetail = async news_id =>{
+//     const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     console.log(data.data);
+// }
+
+
+
+const loadModalDetail = (news_id) => {
+    const url = `https://openapi.programming-hero.com/api/news/${news_id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data.data))
+}
+
+// const displayModal = modals => {
+//     console.log(modals)
+//     const newsDetailModal = document.getElementById('modalDetail');
+//     modals.forEach(modal => {
+//         const modalDiv = document.createElement('div')
+//     modalDiv.classList.add('modal')
+//     modalDiv.innerHTML = `
+//     <div class="modal fade" id="newsDetailModal" tabindex="-1" aria-labelledby="newsDetailModalLabel" aria-hidden="true">
+//       <div class="modal-dialog">
+//         <div class="modal-content">
+//           <div class="modal-header">
+//             <h5 class="modal-title" id="newsDetailModalLabel">Modal title</h5>
+//             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+//           </div>
+//           <div class="modal-body">
+//             ...
+//           </div>
+//           <div class="modal-footer">
+//             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//     `
+//     newsDetailModal.appendChild(modalDiv)
+//     })
+// }
 
 loadCategory();
